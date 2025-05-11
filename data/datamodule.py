a6c4cb7a6800dc60e1fea6a0da6edb427737e69b
Lukas Pelz <lukas.pelz@posteo.de>
2025-05-11 20:52:08 +0200

    Implement dataModule and dataSet

--- data/datamodule.py
+++ data/datamodule.py
@@ -5,7 +5,7 @@ import os
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 
-from dataset import SmallAnimalsDataset
+from data.dataset import SmallAnimalsDataset
 
 # Custom Data Module for Pytorch Lightning
 # Source: https://pytorch-lightning.readthedocs.io/en/1.1.8/introduction_guide.html#data
@@ -34,11 +34,26 @@ class Animal_DataModule(pl.LightningDataModule):
     """
     
     def __init__(self, data_dir: str, batch_size: int = 1):
+        """
+        Initializes the data module with the specified data directory and batch size.
+
+        Args:
+            data_dir (str): Path to the directory containing the data.
+            batch_size (int, optional): Number of samples per batch. Defaults to 1.
+        """
         super().__init__()
         self.data_dir = data_dir
         self.batch_size = batch_size
 
     def prepare_data(self):
+        """
+        Prepares the data for use in the data module.
+
+        This method is typically used for tasks such as downloading, splitting, 
+        or preprocessing data. However, in this case, it is not required because 
+        the data is already prepared through the custom dataset. This method is 
+        only called on a single GPU/TPU in distributed settings.
+        """
         # download, split, etc...
         # only called on 1 GPU/TPU in distributed
         # not required because our data is already prepared through the custom dataset
@@ -46,9 +61,19 @@ class Animal_DataModule(pl.LightningDataModule):
 
     # Load the datasets
     def setup(self, stage=None):
-
-        # the datasets instances are generated here, depending on the current stage (val/train/test split)
-
+        """
+        Sets up the datasets for training, validation, and testing stages.
+        Args:
+            stage (str, optional): The current stage of the process. Can be "fit", "test", 
+                or None. Defaults to None.
+                - "fit": Prepares the train and validation datasets.
+                - "test": Prepares the test dataset.
+                - None: Prepares all datasets (train, validation, and test).
+        Attributes:
+            train_dataset (SmallAnimalsDataset): Dataset instance for training.
+            val_dataset (SmallAnimalsDataset): Dataset instance for validation.
+            test_dataset (SmallAnimalsDataset): Dataset instance for testing.
+        """
         # Assign train/val datasets for use in dataloaders
         if stage == "fit" or stage is None:
             self.train_dataset = SmallAnimalsDataset(data_dir=self.data_dir+'\\pd_dataset_train\\')
@@ -62,6 +87,16 @@ class Animal_DataModule(pl.LightningDataModule):
 
     # Define the train dataloader
     def train_dataloader(self):
+        """
+        Creates and returns a DataLoader for the training dataset.
+
+        This DataLoader is configured to load data from the training dataset with the specified
+        batch size, number of worker processes, and shuffling enabled. Persistent workers are
+        used to improve performance by keeping worker processes alive between epochs.
+
+        Returns:
+            DataLoader: A PyTorch DataLoader instance for the training dataset.
+        """
         return DataLoader(
             dataset=self.train_dataset,
             batch_size=self.batch_size,
@@ -72,6 +107,17 @@ class Animal_DataModule(pl.LightningDataModule):
 
     # Define the validation dataloader
     def val_dataloader(self):
+        """
+        Creates and returns a DataLoader for the validation dataset.
+
+        The DataLoader is configured with the validation dataset, batch size, 
+        and number of workers based on the available CPU cores. It is set to 
+        not shuffle the data and uses persistent workers for efficient data 
+        loading.
+
+        Returns:
+            DataLoader: A PyTorch DataLoader instance for the validation dataset.
+        """
         return DataLoader(
             dataset=self.val_dataset,
             batch_size=self.batch_size,
@@ -82,6 +128,21 @@ class Animal_DataModule(pl.LightningDataModule):
 
     # Define the test dataloader
     def test_dataloader(self):
+        """
+        Creates and returns a DataLoader for the test dataset.
+
+        The DataLoader is configured with the following parameters:
+        - `dataset`: The test dataset to load data from.
+        - `batch_size`: The number of samples per batch.
+        - `num_workers`: The number of subprocesses to use for data loading, 
+          set to the number of CPUs available.
+        - `persistent_workers`: Whether to keep data loading workers alive 
+          after the initial dataset loading.
+        - `shuffle`: Whether to shuffle the data (set to False for the test dataset).
+
+        Returns:
+            DataLoader: A DataLoader instance for the test dataset.
+        """
         return DataLoader(
             dataset=self.test_dataset,
             batch_size=self.batch_size,
--- data/dataset.py
+++ data/dataset.py
@@ -5,63 +5,104 @@ import os
 import torch
 import pandas as pd
 import numpy as np
+from skimage import io, transform
 # from scipy.ndimage import gaussian_filter1d
-
-
 
 # Custom Dataset for Pytorch
 # Source: https://pytorch.org/tutorials/beginner/basics/data_tutorial.html
 class SmallAnimalsDataset(torch.utils.data.Dataset):
-    def __init__(self, data_dir: str, label_cols: str = Param.feature_list, data_cols: str = ['raw_data']):
-        self.data_dir = data_dir
-        self.label_cols = label_cols
-        self.data_cols = data_cols
-        self.data = []
 
-        # Generate a list containing all file names in directory
+    # Within the constructor, we define dataset metadata (paths, labels...) and optionally apply transformations, normalization, etc.
+    def __init__(self, data_dir: str, transform=None):
+
+        # Path to the data directory
+        self.data_dir = data_dir        
+        
+        # Generate a list containing all file names in the given directory
         self.file_list = os.listdir(data_dir)
+
+        # Transformations to be applied to the data
+        self.transform = transform
+
+
+
 
         # TODO: Change code to allow dynamic loading of data instead of loading all data at once
         # TODO: Change code to use images instead of csv files
 
         # Read all files in directory and store them in a list
-        for file in self.file_list:
+        # for file in self.file_list:
 
             # Read data from csv file
-            temp_data = pd.read_csv(data_dir + file)
+            # temp_data = pd.read_csv(data_dir + file)
             
             # add gaussian distribution over peaks with width of 10
             # reason: the loss function can handle the peaks better when they have a larger range / area for the loss function to work with
-            for feature in Param.feature_list:
-                if(feature == 'P-peak' or feature == 'R-peak' or feature == 'T-peak'):
+            # for feature in Param.feature_list:
+            #     if(feature == 'P-peak' or feature == 'R-peak' or feature == 'T-peak'):
                     
-                    # add gaussian distribution over peaks with width of 10 // use constant to extend data by 0s when filtering with guassian
-                    temp_data[feature] = gaussian_filter1d(np.float64(temp_data[feature]), sigma=10, mode='constant')
-                    # normalize between 0 and 1
-                    max_val = max(temp_data[feature])
-                    if(max_val > 0):
-                        temp_data[feature] = temp_data[feature] * (1/max_val)
+            #         # add gaussian distribution over peaks with width of 10 // use constant to extend data by 0s when filtering with guassian
+            #         # temp_data[feature] = gaussian_filter1d(np.float64(temp_data[feature]), sigma=10, mode='constant')
+            #         # normalize between 0 and 1
+            #         max_val = max(temp_data[feature])
+            #         if(max_val > 0):
+            #             temp_data[feature] = temp_data[feature] * (1/max_val)
 
-                    # Print Data with matplotlib
-                    #import matplotlib.pyplot as plt
-                    #plt.plot(temp_data[feature])
-                    #plt.show()
+            #         # Print Data with matplotlib
+            #         #import matplotlib.pyplot as plt
+            #         #plt.plot(temp_data[feature])
+            #         #plt.show()
             
             # add data to list
-            self.data.append(temp_data)
+            # self.data.append(temp_data)
 
-            # Print the amount of loaded files every 1000 files for better overview during loading
-            if len(self.data) % 1000 == 0:
-                print(f"DATASET: Loaded {len(self.data)} of {len(self.file_list)} files")
+            # # Print the amount of loaded files every 1000 files for better overview during loading
+            # if len(self.data) % 1000 == 0:
+            #     print(f"DATASET: Loaded {len(self.data)} of {len(self.file_list)} files")
 
-    # Return the length of the dataset
+    # Return the total number of samples of the dataset
     def __len__(self):
+        """
+        Returns the total number of files in the dataset.
+
+        This method overrides the `__len__` special method to provide the length
+        of the dataset, which is determined by the number of entries in the 
+        `file_list` attribute.
+
+        Returns:
+            int: The number of files in the dataset.
+        """
         return len(self.file_list)
     
-    # Return a single data entry using a given index
+    # https://docs.pytorch.org/tutorials/beginner/data_loading_tutorial.html
+    # Called by the DataLoader to get a sample from the dataset using a given index
     def __getitem__(self, idx):
+        """
+        Retrieves a single data sample from the dataset at the specified index.
+        Args:
+            idx (int or torch.Tensor): The index of the data sample to retrieve. If a 
+                torch.Tensor is provided, it will be converted to a Python list.
+        Returns:
+            dict: A dictionary containing the following keys:
+                - 'image': The loaded image corresponding to the specified index.
+                - 'label': The label associated with the image. Currently, this is 
+                  set to 0 as a placeholder. Label loading from the file path needs 
+                  to be implemented.
+        """
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
 
-        # TODO: Implement dynamic image data loading here
+        image_path = os.path.join(self.data_dir, self.file_list[idx])
+        image = io.imread(image_path)
+        
+        # TODO: Implement label loading from file path
+        raise NotImplementedError("This method is not yet implemented.")
 
-        data_idx = self.data[idx]
-        return torch.tensor(data_idx[self.data_cols].values).T, torch.tensor(data_idx[self.label_cols].values).T
+        sample = {'image': image, 'label': 0}
+
+        # Apply transformations to the sample if any are defined
+        if self.transform:
+            sample = self.transform(sample)
+
+        # Return the sample
+        return sample
