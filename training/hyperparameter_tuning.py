be4c6a5d5df06a155f33107dcb6fbe6788cf18cf
Lukas Pelz <61017369+lukpausr@users.noreply.github.com>
2025-06-29 20:38:25 +0200

    und das auch noch

--- training/hyperparameter_tuning.py
+++ training/hyperparameter_tuning.py
@@ -11,7 +11,9 @@ import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.getcwd(), '..')))
 from data.datamodule import BinaryImageDataModule
+from data.datamodule import MultiClassImageDataModule
 from models.model_transferlearning import TransferLearningModule
+from models.model_facedetection import TransferLearningModuleMulticlass
 from models.model_cnn import CnnModel
 
 class OptunaTrainer:
@@ -322,6 +324,115 @@ class CnnOptunaTrainer(OptunaTrainer):
 
         # Setup Wandb Logger
         wandb_logger = self._setup_wandb_logger()   
+
+        # Setup trainer
+        trainer = Trainer(
+            max_epochs=self.config['max_epochs'],
+            precision=self.config['precision'],
+            accumulate_grad_batches=self.config['accumulate_grad_batches'],
+            accelerator="auto",
+            devices="auto",
+            strategy="auto",
+            callbacks=[EarlyStopping(monitor="val_loss", patience=3, mode='min')],
+            logger=wandb_logger,
+            enable_progress_bar=False,
+            log_every_n_steps=10,
+        )
+
+        # Train & handle exceptions
+        try:
+            trainer.fit(model=lightning_model, datamodule=dm)
+            checkpoint_path = f"checkpoints/{self.config['wandb_experiment_name']}.ckpt"
+            trainer.save_checkpoint(checkpoint_path)
+        except Exception as e:
+            print(f"Error during training: {e}")
+            wandb.finish()
+            return float("inf")
+
+        val_loss = trainer.callback_metrics.get("val_loss")
+        wandb.finish()
+
+        print(f"Optuna Validation loss: {val_loss.item() if val_loss else 'N/A'}")
+        return val_loss.item() if val_loss else float("inf")
+    
+class FDOptunaTrainer(OptunaTrainer):
+    def __init__(self, model, config, normalize_mean=[0.485, 0.456, 0.406], normalize_std=[0.229, 0.224, 0.225], dataset_name="DwarfRabbits-binary"):
+        super().__init__(model, config, normalize_mean, normalize_std, dataset_name)
+
+    def _build_transform(self, image_size):
+        """
+        Builds a torchvision transformation pipeline for preprocessing images.
+
+        Args:
+            image_size (int): The target size (height and width) to which images will be resized.
+
+        Returns:
+            torchvision.transforms.Compose: A composed transform that applies the following steps:
+                1. Center crops the image to a square using a custom transform.
+                2. Resizes the image to (image_size, image_size).
+                3. Converts the image to a tensor.
+                4. Normalizes the tensor using ImageNet mean and standard deviation.
+        """
+        return v2.Compose([
+            # custom_transforms.CenterCropSquare(),
+            v2.Resize((image_size, image_size)),
+            v2.ToTensor(),
+            v2.Normalize(mean=self.normalize_mean, std=self.normalize_std)
+        ])
+    
+    def run_training(self, trial):
+        """
+        Runs a single training session for hyperparameter optimization using the provided Optuna trial.
+        This method suggests and sets various hyperparameters using the Optuna trial object, prepares the data module and model,
+        sets up logging with Weights & Biases, and initializes the PyTorch Lightning Trainer. It then trains the model and saves
+        the checkpoint. If an exception occurs during training, it logs the error and returns infinity as the loss.
+        Args:
+            trial (optuna.trial.Trial): The Optuna trial object used to suggest hyperparameters.
+        Returns:
+            float: The validation loss after training, or float("inf") if training failed.
+        """
+        # Suggest hyperparameters
+        self.config['batch_size'] = trial.suggest_categorical("batch_size", [32, 64, 128])
+        self.config['image_size'] = trial.suggest_categorical("image_size", [128, 224, 300])  # If not able to use variable image size, set image size to fixed value
+        # self.config['image_size'] = self.config['image_size']
+
+        self.config['max_epochs'] = trial.suggest_int("max_epochs", 20, 40)
+        self.config['accumulate_grad_batches'] = trial.suggest_categorical("accumulate_grad_batches", [1, 2, 4])
+        self.config['precision'] = trial.suggest_categorical("precision", ["16-mixed", 32])
+
+        self.config['optimizer'] = trial.suggest_categorical("optimizer", ["Adam", "SGD", "AdamW"])
+        self.config['learning_rate'] = trial.suggest_float("learning_rate", 1e-4, 1e-2, log=True)
+        self.config['weight_decay'] = trial.suggest_float("weight_decay", 1e-6, 1e-2, log=True)
+
+        self.config['scheduler'] = trial.suggest_categorical("scheduler", ["StepLR", "CosineAnnealingLR", None])
+
+        self.config['model_classifier_layers'] = trial.suggest_int("model_classifier_layers", 1, 2)
+
+        # Prepare transformations and datamodule
+        transform = self._build_transform(self.config['image_size'])
+        dm = MultiClassImageDataModule(
+            data_dir=self.config['path_to_bunnie_data_aug'], 
+            name_list=self.config['name_list'], 
+            transform=transform, 
+            batch_size=self.config['batch_size'], 
+            num_workers=6, 
+            persistent_workers=True
+        )
+
+        # Initialize model
+        self.model, self.config['model_name'] = self.model(amount_of_trainable_linear_layers=self.config['model_classifier_layers'])
+        # Initialize Lightning model
+        lightning_model = TransferLearningModuleMulticlass(
+            self.model,
+            num_classes=len(self.config['name_list']),
+            learning_rate=self.config['learning_rate'],
+            optimizer_name=self.config['optimizer'],
+            weight_decay=self.config['weight_decay'],
+            scheduler_name=self.config['scheduler'],
+        )
+
+        # Setup Wandb Logger
+        wandb_logger = self._setup_wandb_logger()
 
         # Setup trainer
         trainer = Trainer(
